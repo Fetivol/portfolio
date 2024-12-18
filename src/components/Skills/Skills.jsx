fd5368c8bcf3e00db4320b39d3aa0befbd251a00
Fetivol <Fetisowova@gmail.com>
2024-12-18 18:16:48 +0100

    Skills component

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,7 +1,42 @@
 import React from "react";
+import { skills } from "../../data/constants";
+import {
+  Container,
+  Desc,
+  Skill,
+  SkillImage,
+  SkillItem,
+  SkillList,
+  SkillsContainer,
+  SkillTitle,
+  Title,
+  Wrapper,
+} from "./Skills.styled";
 
 const Skills = () => {
-  return <div>Skills</div>;
+  return (
+    <Container id="skills">
+      <Wrapper>
+        <Title>Skills</Title>
+        <Desc>Here are some of my skills on which I working.</Desc>
+        <SkillsContainer>
+          {skills.map((item) => (
+            <Skill key={item.id}>
+              <SkillTitle>{item.title}</SkillTitle>
+              <SkillList>
+                {item.skills.map((skill) => (
+                  <SkillItem key={skill.id}>
+                    <SkillImage src={skill.image} alt="" />
+                    {skill.name}
+                  </SkillItem>
+                ))}
+              </SkillList>
+            </Skill>
+          ))}
+        </SkillsContainer>
+      </Wrapper>
+    </Container>
+  );
 };
 
 export default Skills;
