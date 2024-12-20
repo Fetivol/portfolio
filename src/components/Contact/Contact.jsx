e9cdd4560aaf636c75dd2da1c6578a4549e6ec77
Fetivol <Fetisowova@gmail.com>
2024-12-20 21:00:48 +0100

    Update Contact.jsx

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -30,7 +30,6 @@ const Contact = () => {
 
     const isValidEmail = (email) => {
       const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-      console.log(emailRegex.test(email));
       return emailRegex.test(email);
     };
 
